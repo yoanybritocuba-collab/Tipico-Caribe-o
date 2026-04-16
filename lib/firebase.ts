83ace828ae7365bee97450f3cce79d637fe837d5
yoany-brito <yoanybritocuba@gmail.com>
2026-04-17 00:02:06 +0200

    Configuración independiente para Tipico Caribeño

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,15 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtZzm_wnE_lyi3F8qr8iCQdQA4TSEyozU",
-  authDomain: "gaby-club.firebaseapp.com",
-  projectId: "gaby-club",
-  storageBucket: "gaby-club.firebasestorage.app",
-  messagingSenderId: "1088486906554",
-  appId: "1:1088486906554:web:7a30202f2a92c0010d8083"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Inicializar solo una vez
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
