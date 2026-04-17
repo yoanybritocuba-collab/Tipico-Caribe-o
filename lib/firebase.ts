6ea183e2b26173ef7d6282d78b7214cefd2385c1
yoany-brito <yoanybritocuba@gmail.com>
2026-04-17 12:46:42 +0200

    Hardcodear configuración de Firebase para Tipico Caribeño

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAl_IO_Y-xWCK333pnXQmGiaJ3IuIrdJEk",
+  authDomain: "tipico-caribeno.firebaseapp.com",
+  projectId: "tipico-caribeno",
+  storageBucket: "tipico-caribeno.firebasestorage.app",
+  messagingSenderId: "349147555780",
+  appId: "1:349147555780:web:35372f753cb66d50cca691"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
